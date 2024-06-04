6d7c2bcda5d6bfbdbfea64f2ad83b8b80e748a1d
kooliewale <cooliewale+github@gmail.com>
2024-06-04 23:50:13 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,89 @@
+// In src/index.js 
+const express = require("express"); 
+const bodyParser = require("body-parser");
+const cors=require("cors");
+const axios = require('axios');
+const app = express(); 
+const { createClient } = require('@supabase/supabase-js');
+const PORT = process.env.PORT||4500; 
+const IP='http://127.0.0.1';
+const PROJECT=process.env.PROJECT||'KT';
+const VERSION=process.env.VERSION|'v0.0.1';
+const serverStartTime = new Date();  
+app.use(bodyParser.json());
+app.use(cors());  
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+// Middleware function to log details for all requests
+const logRequest = (req, res, next) => {
+  console.log(`--- Request Details ---`);
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  console.log(`Headers:`);
+  console.dir(req.headers); // Use console.dir for better formatting
+  console.log(`Body:`);
+  console.dir(req.body);   // Access body data for POST requests (if applicable)
+
+  // Allow further processing in the request-response cycle
+  next();
+};
+
+// Apply the middleware to handle all requests
+app.use(logRequest);
+
+const BASE_URL ='https://api.weatherapi.com/v1'
+app.all('*', (req, res) => {
+    let visitordata = {
+        status: res.statusCode,
+        url: req.originalUrl,
+        IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        request_body: req.body,
+        request_method: req.method,
+        lat: req.headers['x-vercel-ip-latitude'],
+        lon: req.headers['x-vercel-ip-longitude'],
+         city: req.headers['x-vercel-ip-city'],
+        region: req.headers['x-vercel-ip-country-region'],
+        country: req.headers['x-vercel-ip-country'],
+        UA: req.headers['user-agent'],
+        date_time: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+        ulid: ulidgen
+        };
+
+   console.log(visitordata);
+    supabase  
+  .from('main-page-visitor')  
+  .insert([visitordata])  
+  .then(response1 => {  
+    console.log('Data sent to Supabase successfully:', response1);  
+  })  
+  .catch(error1 => {  
+    console.error('Error sending data to Supabase:', error1);  
+  });  
+  if (req.url.endsWith('/current.json')) 
+  { 
+    const fullUrl = `${BASE_URL}${req.url}`; // Construct complete URL
+    console.log(fullUrl);
+    axios.get(fullUrl)
+      .then(response => {
+        res.send(response.data); 
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error retrieving data'); // Handle errors gracefully
+      });
+  } else {
+    res.send('GET request received!');
+    console.log('not valid URL');
+  }
+});
+
+
+
+
+app.listen(PORT, () => { 
+    console.log(`${PROJECT} API version ${VERSION} is listening on port ${IP}:${PORT}      ${serverStartTime} `); 
+});
